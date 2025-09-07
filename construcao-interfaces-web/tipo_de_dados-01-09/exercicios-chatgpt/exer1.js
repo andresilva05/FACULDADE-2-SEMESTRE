2f16ebf90e0daafd5a3faa91d94fda7028897773
andresilva05 <andresilva9657@gmail.com>
2025-09-07 19:14:50 -0300

    .

---
+++ construcao-interfaces-web/tipo_de_dados-01-09/exercicios-chatgpt/exer1.js
@@ -0,0 +1,29 @@
+// <!-- ✅ Exercício 1 – Tipos Dinâmicos
+
+// Crie uma variável "dados" e atribua a ela:
+
+// Um número
+
+// Uma string
+
+// Um valor booleano
+
+// Depois, exiba no console o valor e o tipo (typeof) a cada mudança.
+
+// Exemplo esperado:
+
+// Valor: 10 | Tipo: number
+// Valor: Olá | Tipo: string
+// Valor: true | Tipo: boolean -->
+
+let dados;
+
+dados = 10;
+console.log("Valor: " + dados + " | Tipo: " + typeof dados);
+
+dados = "Olá";
+
+console.log("Valor: " + dados + " | Tipo: " + typeof dados);
+
+dados = true;
+console.log("Valor: " + dados + " | Tipo: " + typeof dados);
---
+++ construcao-interfaces-web/tipo_de_dados-01-09/exercicios-chatgpt/exer3.js
@@ -0,0 +1,22 @@
+// ✅ Exercício 3 – Arrays
+
+// Crie um array com 3 frutas e:
+
+// Mostre a primeira fruta.
+
+// Adicione mais uma fruta no final.
+
+// Exiba o array atualizado.
+
+// Saída esperada:
+
+// Primeira fruta: maçã
+// Array final: maçã, banana, laranja, uva
+
+let frutas = ["maçã", " banana", " laranja"];
+
+console.log("Primeira fruta: " + frutas[0]);
+
+frutas[3] = "uva";
+
+console.log("Array final: " + frutas);
